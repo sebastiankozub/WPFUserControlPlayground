502697cbbcd47b77faed47111862387bb2d0818e
Sebastian Kozub <sebastiankozub@o2.pl>
2020-04-25 07:55:24 +0100

    Add project files.

---
+++ WpfUserControlLibrary/TriggerListAdd.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfUserControlLibrary
+{
+    public partial class TriggerListAdd : UserControl
+    {
+        public TriggerListAdd()
+        {
+            InitializeComponent();
+        }
+
+        public static DependencyProperty GetOutputFromInputProperty =
+            DependencyProperty.Register("GetOutputFromInput", 
+                typeof(Func<string, Task<string>>), 
+                typeof(TriggerListAdd));
+
+        public Func<string, Task<string>> GetOutputFromInput
+        {
+            get { return (Func<string, Task<string>>)GetValue(GetOutputFromInputProperty); }
+            set { SetValue(GetOutputFromInputProperty, value); }
+        }
+
+        public static DependencyProperty RowsProperty =
+            DependencyProperty.Register("ControlRows",
+                typeof(ICollection<Row>),
+                typeof(TriggerListAdd),
+                new PropertyMetadata(new ObservableCollection<Row>()));// RowsCollectionChanged));
+
+        public ICollection<Row> ControlRows
+        {
+            get { return (ICollection<Row>)GetValue(RowsProperty); }
+            set { SetValue(RowsProperty, value); }
+        }
+
+        public static DependencyProperty TextableProperty =
+            DependencyProperty.Register("Textable", 
+                typeof(string), 
+                typeof(TriggerListAdd),
+                new PropertyMetadata("Default Output"));
+        public string Textable
+        {
+            get { return (string)GetValue(TextableProperty); }
+            set { SetValue(TextableProperty, value); }
+        }
+
+        private void ButtonGetOutput_Click(object sender, RoutedEventArgs e)
+        {
+            ControlRows.Add(new Row(TextboxInput.Text, GetOutputFromInput));
+        }
+    }
+
+    public class Row : INotifyPropertyChanged
+    {
+        public Row(string input, Func<string, Task<string>> outputFunc)
+        {
+            _getOutputFromInput = outputFunc;
+            _input = input;
+        }
+
+        public Row() { }
+
+        public Row(Func<string, Task<string>> outputFunc)
+        {
+            _getOutputFromInput = outputFunc;
+        }
+
+        public string Input
+        {
+            get
+            {
+                return _input;
+            }
+
+            set
+            {
+                if (_input != value)
+                {
+                    _input = value;
+                    RaisePropertyChanged("Input");
+                    RaisePropertyChanged("Output");
+                }
+            }
+        }
+
+        private readonly Func<string, Task<string>> _getOutputFromInput;
+
+        public string Output
+        {
+            get
+            {
+                if (_getOutputFromInput != null)
+                    _getOutputFromInput(_input).ContinueWith((t) => { _output = t.Result; RaisePropertyChanged("Output"); });
+                return _output;
+            }
+            set
+            {
+                _output = value;
+                RaisePropertyChanged("Output");
+            }
+        }
+
+        private string _input;
+        private string _output;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ WpfUserControlsShowcase/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfUserControlLibrary;
+
+namespace WpfUserControlsShowcase
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelfMainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Rows.Add(new Row("999 222,00", GetOutput));
+            Rows.Add(new Row("322 444 666,88", GetOutput));
+            Rows.Add(new Row("465 444 666,99", GetOutput));
+            Rows.Add(new Row("282 222,99", GetOutput));
+            Rows.Add(new Row("666 555 888", GetOutput));
+            Rows.Add(new Row("322 444 666,88", GetOutput));
+            Rows.Add(new Row("222 212,99", GetOutput));
+            Rows.Add(new Row("999 555 888", GetOutput));
+            Rows.Add(new Row(GetOutput));
+            Rows.Add(new Row(GetOutput));
+        }
+
+        public DependencyProperty GetOutputProperty =
+            DependencyProperty.Register("GetOutput",
+                typeof(Func<string, Task<string>>),
+                typeof(MainWindow),
+                new PropertyMetadata(new Func<string, Task<string>>(GetOutputTemp)));
+
+        public Func<string, Task<string>> GetOutput
+        {
+            get { return (Func<string, Task<string>>)GetValue(GetOutputProperty); }
+            set { SetValue(GetOutputProperty, value); }
+        }
+
+        private static Func<string, Task<string>> GetOutputTemp = new Func<string, Task<string>>(
+            async (s) => await Task.Run(
+                () => { var r = s + " " + s + " " + s; Thread.Sleep(1000); return r; }
+        ));
+
+        public DependencyProperty RowsProperty =
+            DependencyProperty.Register("Rows",
+            typeof(ICollection<Row>),
+            typeof(TriggerListAdd),
+            new PropertyMetadata(new ObservableCollection<Row>()));
+
+        public ICollection<Row> Rows
+        {
+            get { return (ICollection<Row>)GetValue(RowsProperty); }
+            set { SetValue(RowsProperty, value); }
+        }
+    }
+}
